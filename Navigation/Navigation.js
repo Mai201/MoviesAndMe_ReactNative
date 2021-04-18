8011a0c16824b7d44d8e04bb04bb289fa1a49394
Mai201 <64534832+Mai201@users.noreply.github.com>
2021-04-18 16:36:02 +0200

    Update app
    Same as solution expo
    + sconf android
    + add Avatar
    + add News
    + add persist

---
+++ Navigation/Navigation.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Image } from 'react-native';
+
+import Search from '../Components/Search';
+import FilmDetail from '../Components/FilmDetail';
+import Favorites from '../Components/Favorites';
+import News from '../Components/News';
+
+const Stack = createStackNavigator();
+const Tab= createBottomTabNavigator();
+
+function SearchNavig() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Search' 
+                component={Search} 
+                options={{ title:'Rechercher' }}
+            />
+            <Stack.Screen
+                name='FilmDetail'
+                component={FilmDetail}
+                options={{ title:'Détail du film' }}
+            />
+        </Stack.Navigator>
+    ) 
+}
+
+function FavoritesNavig() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Favorites' 
+                component={Favorites} 
+                options={{ title:'Favoris' }}
+            />
+            <Stack.Screen
+                name='FilmDetail'
+                component={FilmDetail}
+                options={{ title:'Détail du film' }}
+            />
+        </Stack.Navigator>
+    ) 
+}
+
+function NewsNavig() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name='News' 
+                component={News} 
+                options={{ title:'Les derniers films' }}
+            />
+            <Stack.Screen
+                name='FilmDetail'
+                component={FilmDetail}
+                options={{ title:'Détail du film' }}
+            />
+        </Stack.Navigator>
+    ) 
+}
+
+export default function MyTabs() {
+    return(
+        <NavigationContainer>
+            <Tab.Navigator 
+                tabBarOptions={{ 
+                    activeBackgroundColor: '#DDDDDD', 
+                    inactiveBackgroundColor: '#FFFFFF',
+                    showLabel: false, 
+                    showIcon: true
+                }}
+            >
+                <Tab.Screen 
+                    name="SearchNavig" 
+                    component={SearchNavig}
+                    options={{ tabBarIcon: () => {
+                        return <Image
+                            source={require('../Images/ic_search.png')}
+                            style={styles.icon}/>
+                    } }} 
+                />
+                <Tab.Screen 
+                    name="Favorites" 
+                    component={FavoritesNavig}
+                    options={{ tabBarIcon: () => {
+                        return <Image 
+                            source={require('../Images/ic_favorite.png')}
+                            style={styles.icon}/>
+                    } }} 
+                />
+                <Tab.Screen 
+                    name="News" 
+                    component={NewsNavig}
+                    options={{ tabBarIcon: () => {
+                        return <Image 
+                            source={require('../Images/ic_fiber_new.png')}
+                            style={styles.icon}/>
+                    } }} 
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
+
+const styles = StyleSheet.create({
+    icon: {
+      width: 30,
+      height: 30
+    }
+  })
+
+
---
+++ Store/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+import toggleFavorite from './Reducers/favoriteReducer';
+import setAvatar from './Reducers/avatarReducer'
+
+import { persistCombineReducers } from 'redux-persist'
+//import storage from 'redux-persist/lib/storage' = ancien code
+import AsyncStorage from '@react-native-community/async-storage'
+
+const rootPersistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+}
+
+
+export default createStore(persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}));
+
